8352ba5900cf1fdcb960e6b869e1fee0de4a92af
Ashugeo <hugo.saintemarie@gmail.com>
2019-03-01 18:47:35 +0100

    Implement map system

--- js/main.js
+++ js/main.js
@@ -2,11 +2,25 @@ const gridSize = 40;
 const rows = 10;
 const cols = 10;
 
+const debug = false;
+
+const colors = ['#dddddd', '#4caf50', '#2196f3', '#ffc107'];
+
 let cells = [];
 
 let hoveredCell = {};
 let clickedCell = {};
 let selectedCell = {};
+
+let board;
+let hero;
+
+function preload() {
+    const data = loadJSON('data/map1.json', () => {
+        board = data.board;
+        hero = new Hero(data.hero.pos.x, data.hero.pos.y, data.hero.dir);
+    });
+}
 
 function setup() {
     const canvas = createCanvas(window.innerWidth / 2, window.innerHeight);
@@ -14,9 +28,10 @@ function setup() {
 
     for (let y = 0; y < rows; y += 1) {
         for (let x = 0; x < cols; x += 1) {
-            cells.push(new Cell(x, y));
+            cells.push(new Cell(x, y, board[y] ? board[y][x] : false));
         }
     }
+
 }
 
 function draw() {
@@ -30,12 +45,34 @@ function draw() {
 
     translate(oX, oY);
     for (const cell of cells) cell.display();
+    hero.display();
+}
+
+class Hero {
+    constructor(x, y, dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    display() {
+        rect(gridSize / 2 + this.x, gridSize / 2 + this.y, 10, 10);
+    }
+
+    walk() {
+        // this.x += [][this.dir]
+    }
 }
 
 class Cell {
-    constructor(x, y) {
+    constructor(x, y, type) {
         this.x = x;
         this.y = y;
+
+        if (type && type !== '') {
+            this.type = parseInt(type.match(/\d/)[0]);
+            this.star = !!type.match(/[s]/);
+        }
     }
 
     display() {
@@ -45,14 +82,40 @@ class Cell {
         push();
         translate(x, y);
 
-        rect(0, 0, gridSize, gridSize);
+        if (this.type !== undefined) {
+            fill(colors[this.type]);
+            rect(0, 0, gridSize, gridSize);
+        }
 
-        fill(0);
-        noStroke();
-        text(`${this.x}:${this.y}`, 5, 15);
+        if (this.star) {
+            fill('#faf030');
+            stroke('#7a7010');
+            star(gridSize / 2, gridSize / 2, 5, 15, 5);
+        }
+
+        if (debug) {
+            fill(0);
+            noStroke();
+            text(`${this.x}:${this.y}`, 5, 15);
+        }
 
         pop();
     }
+}
+
+function star(x, y, radius1, radius2, npoints) {
+    const angle = TWO_PI / npoints;
+
+    beginShape();
+    for (let a = radians(-90); a < TWO_PI - radians(90); a += angle) {
+        let sx = x + cos(a) * radius2;
+        let sy = y + sin(a) * radius2;
+        vertex(sx, sy);
+        sx = x + cos(a + angle / 2) * radius1;
+        sy = y + sin(a + angle / 2) * radius1;
+        vertex(sx, sy);
+    }
+    endShape(CLOSE);
 }
 
 $(document).on('click', '.step', e => {
